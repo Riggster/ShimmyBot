7dfab2891f7e1926ba981b54ae3e91589a37b26c
Ryan Rigby <john.rigby@us.af.mil>
2024-11-23 22:19:46 -0700

    initial commit - new spring initializr project

---
+++ src/main/java/com/shimmybot/ShimmybotApplication.java
@@ -0,0 +1,13 @@
+package com.shimmybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimmybotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimmybotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shimmybot/ShimmybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimmybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShimmybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
